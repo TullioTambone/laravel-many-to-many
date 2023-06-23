cbc2e9365d07539121b1e6fb3016de0dd628b3dd
TullioTambone <tulliotambone97@gmail.com>
2023-06-23 15:29:09 +0200

    visulizzazione nella show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,24 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <ul>
-        <li>
-            {{ $project->title }}
-            <!-- <img src="{{ $project->img }}" alt=""> -->
-            <img class="img-fluid" src="{{asset('storage/' . $project->img)}}" alt="">
-            @if($project->type)
-            <h6>type: {{$project->type->name}}</h6>
+    <div>
+
+        <div>
+            Technology: 
+            @if ($project->technologies)
+                @foreach($project->technologies as $elem)
+                    {{$elem->name}}
+                @endforeach
             @endif
-            
-            <div>
-                <a href="{{ route('admin.projects.edit', $project) }}">modifica</a>
-                <form action="{{ route('admin.projects.destroy', $project) }}" method="POST" class="ms-3">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger" type="submit">elimina</button>
-                </form>
-            </div>
-        </li>
-    </ul>
+        </div>
+
+
+        {{ $project->title }}
+        
+        <!-- <img src="{{ $project->img }}" alt=""> -->
+        <img class="img-fluid" src="{{asset('storage/' . $project->img)}}" alt="">
+        @if($project->type)
+        <h6>type: {{$project->type->name}}</h6>
+        @endif
+        
+        <div>
+            <a href="{{ route('admin.projects.edit', $project) }}">modifica</a>
+            <form action="{{ route('admin.projects.destroy', $project) }}" method="POST" class="ms-3">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger" type="submit">elimina</button>
+            </form>
+        </div>
+    </div>
+
 
 @endsection
